34ee0e14ca74593687357ad774a933a9b5fec2d2
Alexander Armua <alexanderarmua1@gmail.com>
2022-09-16 12:30:27 -0300

    Added mazeRoute testcases

---
+++ src/mazeRoute.test.ts
@@ -0,0 +1,44 @@
+import { Maze, MazeChar, Position } from "./global/types";
+import possibleRoutes from "./mazeRoute";
+
+describe("MazeRoute", () => {
+    const maze: Maze = [
+        ["B", "C", "A"],
+        ["A", "C", "A"],
+        ["B", "C", "A"],
+    ];
+
+    const mustFollowRoute: MazeChar[] = ["C", "C", "C"];
+
+    test('possibleRoutes should return a path to right', () => {
+        const position: Position = { x: 0, y: 0 };
+    
+        let steps = 1;
+
+        const nextRoute = possibleRoutes(position, maze, steps, mustFollowRoute);
+
+        expect(nextRoute).toEqual([{ x: 0, y: 1 }]);
+    });
+
+    test('possibleRoutes should return a path to bottom and up', () => {
+        const position: Position = { x: 0, y: 1 };
+    
+        let steps = 2;        
+
+        const nextRoute = possibleRoutes(position, maze, steps, mustFollowRoute);
+
+        expect(nextRoute).toContainEqual({ x: 0, y: 0 });
+        expect(nextRoute).toContainEqual({ x: 1, y: 1 });
+    });
+
+    test('possibleRoutes should return a path to left and up', () => {
+        const position: Position = { x: 2, y: 1 };
+    
+        let steps = 3;
+
+        const nextRoute = possibleRoutes(position, maze, steps, mustFollowRoute);
+
+        expect(nextRoute).toContainEqual({ x: 1, y: 1 });
+        expect(nextRoute).toContainEqual({ x: 2, y: 0 });
+    });
+});
